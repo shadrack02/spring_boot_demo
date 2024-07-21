7329f9a2417335093d9f99c4e6528f8f673ca5df
shadrack02 <shadrackyunzu@gmail.com>
2024-07-21 23:47:19 +0300

    first commit

---
+++ sprdemo/src/main/java/com/yunzu/sprdemo/SprDemoApplication.java
@@ -0,0 +1,13 @@
+package com.yunzu.sprdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprDemoApplication.class, args);
+	}
+
+}
---
+++ sprdemo/src/test/java/com/yunzu/sprdemo/SprdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yunzu.sprdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
